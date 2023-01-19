f9d251fe70f92f70701b5984b22b0530b093a6da
Lum Naw <lumnaw07@gmail.com>
2023-01-19 14:32:00 +0900

    Initial Commit

---
+++ TreeAnimationSwiftUI/TreeAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeAnimationSwiftUIApp.swift
+//  TreeAnimationSwiftUI
+//
+//  Created by cmStudent on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct TreeAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
